69bb567c35c8086ed01b15d93e8ff484197b357b
Carlos <nashlasterby@gmail.com>
2022-09-29 23:58:18 -0300

    feat: foundation-stone, draft of main logic

---
+++ main.py
@@ -0,0 +1,20 @@
+from src.helpers.game_number import *
+from src.helpers.total_kills import *
+from src.helpers.match_players import *
+
+def main():
+  results = []
+  all_games = game_number()
+  for game in all_games:
+    all_kills = total_kills(game)
+    all_players = match_players(game)
+    all_frags = {'FRAG', 'UHUL'}
+    results.append({
+      "game": game.get('game'),
+      "total_kills": all_kills,
+      "players": all_players,
+      "kills": all_frags,
+    })
+  return results
+
+print(main())
