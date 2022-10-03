8afa3d3504eeb060f728186ff22b8bc15de29307
Carlos <nashlasterby@gmail.com>
2022-10-03 19:31:33 -0300

    refactor: include proper object info

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import json
+from collections import Counter
 from src.helpers.game_number import *
 from src.helpers.total_kills import *
 from src.helpers.match_players import *
 from src.helpers.player_kills import *
+from src.helpers.decrement_kills import *
 
 def main():
   results = []
@@ -9,13 +12,22 @@ def main():
   for game in all_games:
     all_kills = total_kills(game)
     all_players = match_players(game)
-    all_frags = player_kills(game)
+    frags = player_kills(game)
+    penalties = decrement_kills(game)
+    scoreOp = {key: frags[key] + penalties.get(key, 0) for key in frags}
+    score = Counter(scoreOp).most_common()
     results.append({
       "game": game.get('game'),
       "total_kills": all_kills,
       "players": all_players,
-      "kills": all_frags,
+      "kills": dict(score),
     })
-  return results
+  return json.dumps(results)
 
 print(main())
+
+# resolver pq em "kills" só aparece uma parte da string dos nomes dos players naquele dict OK
+
+# incluir o decremento na kill por jogador caso o <world> os mate
+
+# incluir função semelhante, por fora, de jogos e suas causas de morte, com contagem OK
