592f489bb4dd96f150d5b74746acf5834eac1be8
Carlos <nashlasterby@gmail.com>
2022-10-03 18:46:06 -0300

    feat: context API files

---
+++ front-end/src/context/Provider.js
@@ -0,0 +1,40 @@
+import { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function Provider({ children }) {
+  const [matches, setMatches] = useState([]);
+  const [deathCauses, setDeathCauses] = useState();
+  const [selected, setSelected] = useState(1);
+  const [selectedCauses, setSelectedCauses] = useState(1);
+
+  const value = useMemo(() => (
+    {
+      matches,
+      setMatches,
+      deathCauses,
+      setDeathCauses,
+      selected,
+      setSelected,
+      selectedCauses,
+      setSelectedCauses,
+    }
+  ), [
+      deathCauses, setDeathCauses,
+      matches, setMatches,
+      selected, setSelected,
+      selectedCauses, setSelectedCauses
+    ]);
+
+  return (
+    <Context.Provider value={ value }>
+      { children }
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.objectOf(PropTypes.any),
+}.isRequired;
+
+export default Provider;
