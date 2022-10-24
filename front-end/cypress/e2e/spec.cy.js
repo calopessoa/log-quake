950c5dfee731c5b479f9e3571499926355dbfc8a
Carlos <nashlasterby@gmail.com>
2022-10-24 11:00:03 -0300

    feat: tests select text elements

--- front-end/cypress/e2e/spec.cy.js
+++ front-end/cypress/e2e/spec.cy.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-undef */
 describe('empty spec', () => {
   it('passes', () => {
-    // eslint-disable-next-line no-undef
     cy.visit('https://quake3-log-viewer.vercel.app/')
-  })
-})
+  });
+
+  it('must render `Kills per player` on the main screen', () => {
+    cy.get('.select-left').should('contain', 'Kills per player')
+  });
+  it('must render `Causes of Death` on the main screen', () => {
+    cy.get('.select-right').should('contain', 'Causes of Death')
+  });
+
+});
